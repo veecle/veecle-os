dd061de782b2d4de31998bf41fd6871c542ef1e9
Julian Schindel <julian.schindel@veecle.io>
2025-12-05 10:45:06 +0100

    Wip: now with less Pin and more unsafe
    Signed-off-by: Julian Schindel <julian.schindel@veecle.io>

--- veecle-os-runtime/src/find.rs
+++ veecle-os-runtime/src/find.rs
@@ -6,24 +6,24 @@ use core::mem::transmute;
 use core::pin::Pin;
 
 pub trait Find<'a> {
-    fn find<T>(&self) -> Option<Pin<&'a Slot<T>>>
+    fn find<T>(&self) -> Option<&'a Slot<T>>
     where
         T: Storable + Sized + 'static;
-    fn find_ref<T>(&self) -> Option<&Pin<&'a Slot<T>>>
+    fn find_ref<T>(&self) -> Option<&'a Slot<T>>
     where
         T: Storable + Sized + 'static;
 }
 
 impl<'a> Find<'a> for () {
     #[inline(always)]
-    fn find<T>(&self) -> Option<Pin<&'a Slot<T>>>
+    fn find<T>(&self) -> Option<&'a Slot<T>>
     where
         T: Storable + Sized + 'static,
     {
         None
     }
     #[inline(always)]
-    fn find_ref<T>(&self) -> Option<&Pin<&'a Slot<T>>>
+    fn find_ref<T>(&self) -> Option<&'a Slot<T>>
     where
         T: Storable + Sized + 'static,
     {
@@ -31,25 +31,25 @@ impl<'a> Find<'a> for () {
     }
 }
 
-impl<'a, X, Y> Find<'a> for (&Pin<&'a Slot<X>>, Y)
+impl<'a, X, Y> Find<'a> for (&'a Slot<X>, Y)
 where
     X: Storable,
     Y: Find<'a>,
 {
     #[inline(always)]
-    fn find<T>(&self) -> Option<Pin<&'a Slot<T>>>
+    fn find<T>(&self) -> Option<&'a Slot<T>>
     where
         T: Storable + Sized + 'static,
     {
         if TypeId::of::<X>() == TypeId::of::<T>() {
-            Some(self.0.assert_is_type())
+            Some(unsafe { transmute(self.0) })
         } else {
             Find::find(&self.1)
         }
     }
 
     #[inline(always)]
-    fn find_ref<T>(&self) -> Option<&Pin<&'a Slot<T>>>
+    fn find_ref<T>(&self) -> Option<&'a Slot<T>>
     where
         T: Storable + Sized + 'static,
     {
@@ -58,27 +58,6 @@ where
         } else {
             Find::find_ref(&self.1)
         }
-    }
-}
-
-impl<'a, Y> Find<'a> for ((), Y)
-where
-    Y: Find<'a>,
-{
-    #[inline(always)]
-    fn find<T>(&self) -> Option<Pin<&'a Slot<T>>>
-    where
-        T: Storable + Sized + 'static,
-    {
-        Find::find(&self.1)
-    }
-
-    #[inline(always)]
-    fn find_ref<T>(&self) -> Option<&Pin<&'a Slot<T>>>
-    where
-        T: Storable + Sized + 'static,
-    {
-        Find::find_ref(&self.1)
     }
 }
 
@@ -96,7 +75,7 @@ where
     where
         T: Storable + 'static,
     {
-        self.1.find().unwrap()
+        unsafe { Pin::new_unchecked(self.1.find().unwrap()) }
     }
 }
 
@@ -126,8 +105,8 @@ macro_rules! create_locals {
     ($handler:tt, $($t:ty),*) => {
         let tuple = ();
         $(
-            let a = core::pin::pin!($crate::find::make_slot::<$t>());
-            let mut tuple = (&a.as_ref(), tuple);
+            let a = $crate::find::make_slot::<$t>();
+            let mut tuple = (&a, tuple);
             if let Some(a) = $crate::find::Find::find_ref::<$t>(&tuple.1){
                 let x = tuple.0;
                 tuple.0 = a;
