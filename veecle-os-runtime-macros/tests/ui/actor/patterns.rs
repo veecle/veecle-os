2ad3e770dc7798e164254311d745004c07481c31
Nemo157 <wim.looman@veecle.io>
2026-01-13 13:36:58 +0100

    feat: support any patterns for `#[actor]` arguments (#219)
    Closes: DEV-1204, #120
    
    Signed-off-by: Wim Looman <wim.looman@veecle.io>

---
+++ veecle-os-runtime-macros/tests/ui/actor/patterns.rs
@@ -0,0 +1,19 @@
+#[derive(Debug, PartialEq, Clone, Default, veecle_os_runtime::Storable)]
+pub struct Sensor(pub u8);
+
+#[veecle_os_runtime_macros::actor]
+async fn macro_test_actor(
+    _: veecle_os_runtime::Reader<'_, Sensor>,
+    veecle_os_runtime::Writer { .. }: veecle_os_runtime::Writer<'_, Sensor>,
+    #[init_context] (a, [b, c], Sensor { 0: x }): (u8, [u8; 2], Sensor),
+) -> std::convert::Infallible {
+    let _: u8 = a + b + c + x;
+    unreachable!("We only care about the code compiling.");
+}
+
+fn main() {
+    let _ = veecle_os_runtime::execute! {
+        store: [Sensor],
+        actors: [MacroTestActor: (1, [2, 3], Sensor(4))],
+    };
+}
