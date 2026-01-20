c9025ebe2a16deef18eee5925a0f735ebc994be6
arctic-alpaca <67190338+arctic-alpaca@users.noreply.github.com>
2026-01-20 11:33:15 +0100

    refactor: move slot validation to SlotTrait (#236)
    Moves validation logic from `make_store_and_validate` into SlotTrait and
    IntoSlots traits to allow different slot types to define their
    validation logic.
    
    Refs: DEV-437
    
    Signed-off-by: Julian Schindel <julian.schindel@veecle.io>

--- veecle-os-runtime/src/datastore/mod.rs
+++ veecle-os-runtime/src/datastore/mod.rs
@@ -19,3 +19,42 @@ pub use self::reader::Reader;
 pub use self::slot::Storable;
 pub(crate) use self::slot::{Slot, SlotTrait};
 pub use self::writer::Writer;
+
+/// Returns a type that will write the given list of types out comma separated with backtick
+/// quoting, or `nothing` if it is empty.
+///
+/// ```text
+/// [] => "nothing"
+/// ["A"] => "`A`"
+/// ["A", "B"] => "`A`, `B`"
+/// ["A", "B", "C"] => "`A`, `B`, `C`"
+/// ```
+pub(crate) fn format_types(
+    types: impl IntoIterator<Item = &'static str>,
+) -> impl core::fmt::Display {
+    struct Helper<T>(core::cell::RefCell<T>);
+
+    impl<T> core::fmt::Display for Helper<T>
+    where
+        T: Iterator<Item = &'static str>,
+    {
+        fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+            let mut iter = self.0.borrow_mut();
+            if let Some(first) = iter.next() {
+                f.write_str("`")?;
+                f.write_str(first)?;
+                f.write_str("`")?;
+                for next in &mut *iter {
+                    f.write_str(", `")?;
+                    f.write_str(next)?;
+                    f.write_str("`")?;
+                }
+            } else {
+                f.write_str("nothing")?;
+            }
+            Ok(())
+        }
+    }
+
+    Helper(core::cell::RefCell::new(types.into_iter()))
+}
